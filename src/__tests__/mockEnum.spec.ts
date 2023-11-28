c9c94a2ba991a4b10bb8be0258d7d910e80e56b9
Drake Costa <drake@saeris.io>
2023-11-28 13:38:09 -0800

    Fix broken `#getValidEnumValues()` method implementation
    Native TypeScript enums with string values were returning undefined. This resolves the bug and adds some additional test cases for different native enum variants.

--- src/__tests__/mockEnum.spec.ts
+++ src/__tests__/mockEnum.spec.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/prefer-enum-initializers */
+/* eslint-disable @typescript-eslint/prefer-literal-enum-member */
+/* eslint-disable no-bitwise */
 import { describe, expect, it } from "vitest";
 import { parse, parseAsync, enum_, enumAsync } from "valibot";
 import { Valimock } from "../Valimock.js";
@@ -12,12 +15,38 @@ describe(`mockEnum`, () => {
     REJECTED = 3
   }
 
-  it.each([enum_(States)])(`should generate valid mock data (%#)`, (schema) => {
-    const result = mockSchema(schema);
-    expect(parse(schema, result)).toStrictEqual(result);
-  });
+  enum Flags {
+    JOIN = 1 << 0,
+    SPECTATE = 1 << 1,
+    PLAY = 1 << 2
+  }
 
-  it.each([enumAsync(States)])(
+  enum Features {
+    FOO = `FOO`,
+    BAR = `BAR`,
+    BAZ = `BAZ`
+  }
+
+  enum Status {
+    RED,
+    YELLOW,
+    GREEN
+  }
+
+  it.each([enum_(States), enum_(Flags), enum_(Features), enum_(Status)])(
+    `should generate valid mock data (%#)`,
+    (schema) => {
+      const result = mockSchema(schema);
+      expect(parse(schema, result)).toStrictEqual(result);
+    }
+  );
+
+  it.each([
+    enumAsync(States),
+    enumAsync(Flags),
+    enumAsync(Features),
+    enumAsync(Status)
+  ])(
     `should generate valid mock data with async validation (%#)`,
     async (schema) => {
       const result = mockSchema(schema);
